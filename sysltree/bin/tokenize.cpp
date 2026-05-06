f11f46af5fea7b8c2c8119f3dee5043ddaa7a76d
eulegang <eulegang@eulegang.dev>
2026-05-05 20:25:05 -0500

    feat: Adding a tokenizer program

---
+++ sysltree/bin/tokenize.cpp
@@ -0,0 +1,149 @@
+
+#include <getopt.h>
+
+#include <iostream>
+#include <string_view>
+#include <unistd.h>
+#include <vector>
+
+#include <arcana.h>
+#include <chroma.h>
+#include <mfile.h>
+
+#include "tokens.h"
+
+bool lex_file(std::string_view content, const char *path);
+
+bool verbose = false;
+bool color = true;
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+
+  static struct option long_options[] = {
+      {"verbose", no_argument, 0, 'v'},
+      {"help", no_argument, 0, 'h'},
+      {0, 0, 0, 0},
+      // Sentinel to mark the end of the array
+  };
+
+  int c;
+
+  const char *no_color = getenv("NO_COLOR");
+
+  if (no_color && no_color[0] != 0) {
+    color = false;
+  }
+
+  std::vector<std::string> files;
+  while ((c = getopt_long(argc, argv, "vh", long_options, NULL)) != -1) {
+    switch (c) {
+    case 'v':
+      verbose = true;
+      break;
+
+    case 'h':
+      std::cout << "sysl-tokenize [-v] [files...]" << std::endl;
+      return 0;
+      break;
+    }
+  }
+
+  int opt_i = optind;
+  while (opt_i < argc) {
+    files.push_back(argv[opt_i++]);
+  }
+
+  if (!color) {
+    std::cout << chroma::disable;
+  }
+
+  bool success = true;
+  if (files.empty()) {
+    std::string content((std::istreambuf_iterator<char>(std::cin)),
+                        std::istreambuf_iterator<char>());
+    success &= lex_file(content, NULL);
+  } else if (files.size() == 1) {
+    mfile file{files[0]};
+    success &= lex_file(file, NULL);
+  } else {
+    for (const auto &path : files) {
+      mfile file{path.c_str()};
+      success &= lex_file(file, path.c_str());
+    }
+  }
+
+  if (!success) {
+    return 2;
+  }
+
+  return 0;
+}
+
+chroma::basic_t token_color(sysltree::token type) {
+  (void)type;
+  return chroma::cyan;
+}
+
+bool lex_file(std::string_view content, const char *path) {
+  arcana_tokens_options opts = {
+      .content = {.data = content.data(), .len = content.length()},
+      .tokenizer = sysltree::tokenizer,
+  };
+
+  arcana_tokens_error err;
+  arcana_tokens *tokens = arcana_tokens_init(opts, &err);
+
+  if (!tokens) {
+    if (path)
+      std::cerr << path << ":" << std::endl;
+
+    switch (err.err) {
+    case ARCANA_TOKENS_ERROR_MAP:
+      std::cerr << "failed to map memory" << std::endl;
+      break;
+    case ARCANA_TOKENS_ERROR_OVERFLOW:
+      std::cerr << "File to large to tokenize" << std::endl;
+      break;
+    case ARCANA_TOKENS_ERROR_INVALID:
+      std::cerr << "Invalid token found" << std::endl;
+      auto len = content.find_first_of("\n", err.pos);
+      auto sub = content.substr(err.pos, len - 1);
+      std::cerr << sub << std::endl;
+      std::cerr << "^" << std::endl;
+
+      break;
+    }
+
+    return false;
+  }
+
+  const char *pad = "";
+  if (path) {
+    std::cout << path << ":" << std::endl;
+    pad = "  ";
+  }
+
+  size_t len = arcana_tokens_len(tokens);
+  arcana_token *base = arcana_tokens_data(tokens);
+  arcana_linemeta *meta = arcana_tokens_linemeta(tokens);
+
+  for (size_t i = 0; i < len; i++) {
+    sysltree::token type = (sysltree::token)base[i].type;
+    std::string_view text{content.data() + base[i].off, base[i].len};
+
+    if (verbose) {
+      std::cout << pad << token_color(type) << type << "\t" << chroma::yellow
+                << meta[i].line << ":" << meta[i].column << "\t"
+                << chroma::purple << text << chroma::clear << std::endl;
+    } else {
+
+      std::cout << pad << type << std::endl;
+    }
+  }
+
+  arcana_tokens_deinit(tokens);
+
+  return true;
+}
